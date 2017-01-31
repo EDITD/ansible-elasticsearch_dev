3618f0426e938d671be57ded32b3d3f5350aef63
John Spicer <john@editd.com>
2017-01-31 16:33:20 +0000

    Handle different elasticsearch_base_url for version 5

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -2,8 +2,15 @@ def get_major_version(filename):
     return int(filename[14])
 
 
+def get_elasticsearch_base_url(version):
+    if version < 5:
+        return 'https://download.elasticsearch.org/elasticsearch/elasticsearch'
+    return 'https://artifacts.elastic.co/downloads/elasticsearch'
+
+
 class FilterModule(object):
     def filters(self):
         return {
             'get_major_version': get_major_version,
+            'get_elasticsearch_base_url': get_elasticsearch_base_url,
         }
