dfafd991037aedcad5bbf7ffcbd8f7279ee2a3b0
John Spicer <john@editd.com>
2017-01-31 16:33:36 +0000

    Handle openjdk version on ubuntu 16.04

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -8,9 +8,16 @@ def get_elasticsearch_base_url(version):
     return 'https://artifacts.elastic.co/downloads/elasticsearch'
 
 
+def get_openjdk_version(ubuntu_version):
+    if ubuntu_version == '14.04':
+        return 7
+    return 8
+
+
 class FilterModule(object):
     def filters(self):
         return {
             'get_major_version': get_major_version,
             'get_elasticsearch_base_url': get_elasticsearch_base_url,
+            'get_openjdk_version': get_openjdk_version,
         }
