9f127ea136bd9793e86ba770a28079263336c0ba
John Spicer <john@edited.com>
2017-09-21 15:19:05 +0100

    Make a generic es command function for listing, removing, and installing plugins

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -8,13 +8,25 @@ def get_elasticsearch_base_url(version):
     return 'https://artifacts.elastic.co/downloads/elasticsearch'
 
 
-def get_elasticsearch_plugin_install_command(version):
+def get_elasticsearch_generic_command(version, command):
     if version == 1:
-        return '/usr/share/elasticsearch/bin/plugin --install'
+        return '/usr/share/elasticsearch/bin/plugin --{}'.format(command)
     elif version == 2:
-        return '/usr/share/elasticsearch/bin/plugin install'
+        return '/usr/share/elasticsearch/bin/plugin {}'.format(command)
     else:
-        return '/usr/share/elasticsearch/bin/elasticsearch-plugin install'
+        return '/usr/share/elasticsearch/bin/elasticsearch-plugin {}'.format(command)
+
+
+def get_elasticsearch_plugin_list_command(version):
+    return get_elasticsearch_generic_command(version, 'list')
+
+
+def get_elasticsearch_plugin_remove_command(version):
+    return get_elasticsearch_generic_command(version, 'remove')
+
+
+def get_elasticsearch_plugin_install_command(version):
+    return get_elasticsearch_generic_command(version, 'install')
 
 
 class FilterModule(object):
@@ -23,4 +35,5 @@ class FilterModule(object):
             'get_major_version': get_major_version,
             'get_elasticsearch_base_url': get_elasticsearch_base_url,
             'plugin_install_command': get_elasticsearch_plugin_install_command,
+            'plugin_remove_command': get_elasticsearch_plugin_remove_command,
         }
